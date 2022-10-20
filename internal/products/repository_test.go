482b1d9ab7634946b2885b9186fd4c227bc0da6e
MarianoLibre <mariano.macri@mercadolibre.com>
2022-10-20 14:29:16 -0300

    Mock test "UpdateName"

--- internal/products/repository_test.go
+++ internal/products/repository_test.go
@@ -9,50 +9,31 @@ import (
 
 
 
-type StubStore struct{}
+type StubStore struct{
+	mockedData []Product
+	readWasCalled bool
+}
 
-func (s StubStore) Read(data interface{}) error {
+func (s *StubStore) Read(data interface{}) error {
+	s.readWasCalled = true
 	a, ok := data.(*[]Product)
 	if !ok {
 		return errors.New("it failed!")
 	}
-	*a = append(*a, Product{
-			Id: 1,
-			Name: "celu",
-			Colour: "black",
-			Price: 200.5,
-			Stock: 12,
-			Code: "XXX",
-			Published: true,
-			CreatedAt: "today",
-		})
-	*a = append(*a, Product{
-			Id: 2,
-			Name: "celu",
-			Colour: "red",
-			Price: 500.5,
-			Stock: 12,
-			Code: "ZZZ",
-			Published: true,
-			CreatedAt: "today",
-		})
+	*a = s.mockedData
 	return nil
 }
 
-func (s StubStore) Write(data interface{}) error{
+func (s *StubStore) Write(data interface{}) error{
 	return nil
 }
 
 
 func TestGetAll(t *testing.T) {
-	var db StubStore
-	r := NewRepository(db)
-
-	out, err := r.GetAll()
 	expected := []Product{
 		{
 			Id: 1,
-			Name: "celu",
+			Name: "Before updated",
 			Colour: "black",
 			Price: 200.5,
 			Stock: 12,
@@ -71,8 +52,37 @@ func TestGetAll(t *testing.T) {
 			CreatedAt: "today",
 		},
 	}
+	db := &StubStore{mockedData: expected}
+	r := NewRepository(db)
 
-	assert.Nil(t, err, "")
-	assert.Equal(t, expected, out, "")
+	out, err := r.GetAll()
 
+	assert.Nil(t, err)
+	assert.Equal(t, expected, out)
+}
+
+func TestUpdateName(t *testing.T) {
+	data := []Product{
+		{
+			Id: 1,
+			Name: "Before updated",
+			Colour: "black",
+			Price: 200.5,
+			Stock: 12,
+			Code: "XXX",
+			Published: true,
+			CreatedAt: "today",
+		},
+	}
+	expected := data[0]
+	updatedName := "Updated Name"
+	expected.Name = updatedName
+	db := &StubStore{mockedData: data, readWasCalled: false}
+	r := NewRepository(db)
+
+	out, err := r.UpdateName(1, updatedName)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, out)
+	assert.True(t, db.readWasCalled)
 }
